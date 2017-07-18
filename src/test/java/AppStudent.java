250f2fc22cecd9f21e71a3dc6a5183fd6195d496
Annitra <anuta07.05@gmail.com>
2017-07-18 15:06:01 +0300

    Test creating new someone for AppStudents

--- src/test/java/AppStudent.java
+++ src/test/java/AppStudent.java
@@ -1,4 +1,3 @@
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,38 +8,56 @@ import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 /**
  * Created by User on 14.07.2017.
  */
 public class AppStudent {
     WebDriver driver;
-    /*String selector;
-    public Boolean isPresent(String selector)
-    {
-        try {
-            WebElement presentElement = driver.findElement(By.id(selector));
-            return true;
-        }
-        catch (ElementNotFoundException e) {return false; }
-    }
-    public Boolean notPresent(String selector)
-    {
-        try {
-            WebElement presentElement =driver.findElement(By.id(selector));
-            return false;
-        }
-        catch (ElementNotFoundException e) {return true; }
-    }
-    public Boolean isLogIn(String selector)
-    {
-        try {
-            WebElement presentElement = driver.findElement(By.className(selector));
-            return true;
-        }
-        catch (ElementNotFoundException e) {return false; }
-    }*/
 
+    void sleeppy(int sec) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    void AddingSomeone(String nameSelector, String nameInput,String phoneSelector,String phoneInput){
+        WebElement buttomPlus=driver.findElement(By.className("mdi-content-add"));
+        buttomPlus.click();
+
+        WebElement name = driver.findElement(By.cssSelector(nameSelector));
+        name.clear();
+        name.sendKeys(nameInput);
+
+        WebElement phone = driver.findElement(By.cssSelector(phoneSelector));
+        phone.clear();
+        phone.sendKeys(phoneInput);
+    }
+    void Saving(String selector){
+        WebElement saveButton = driver.findElement(By.cssSelector(selector));
+        saveButton.click();
+
+        sleeppy(2);
+        WebElement save2Button = driver.findElement(By.cssSelector(selector));
+        save2Button.click();
+    }
+
+    void foundNewElement(String tagName){
+        List<WebElement> elements = driver.findElements(By.tagName(tagName));
+        WebElement element = elements.get(elements.size()-1);
+        element.click();
+    }
+    boolean IfAddingElementPresent(String input){
+
+        try {
+            WebElement presentElement = driver.findElement(By.cssSelector(input));
+            return true;
+        }
+        catch (org.openqa.selenium.NoSuchElementException e ) {
+            return false; }
+       }
     @BeforeTest
     void setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe"); //tegs <properties> in pom.xml
@@ -52,35 +69,32 @@ public class AppStudent {
 
     @Test(description ="Create new student")
     void TestCreateStudent(){
-        WebElement buttomPlus=driver.findElement(By.className("mdi-content-add"));
-        buttomPlus.click();
+        AddingSomeone("input[id=\'icon_prefix\']","SomeStudent","input[type=\'tel\']","8-xxx-xxx-xx-xx");
 
-        WebElement name = driver.findElement(By.id("icon_prefix"));
-        name.clear();
-        name.sendKeys("SomeStudent");
+        Saving("a.save-btn");
 
-        WebElement phone = driver.findElement(By.id("icon_telephone"));
-        phone.clear();
-        phone.sendKeys("8-xxx-xxx-xx-xx");
-
-        WebElement downArrow = driver.findElement(By.xpath("//input[@value=\"Student\"]"));
-        downArrow.click();
-
-
+        foundNewElement("li");
+        Assert.assertTrue(IfAddingElementPresent("label.active"));
 
     }
-
-
-
+/*
+    @Test(dependsOnMethods = {"TestCreateStudent"},description = "Delete new student")
+    void TestDeleteStudent(){
+        foundNewElement("li");
+        WebElement checkDelete=driver.findElement(By.cssSelector("i.remove"));
+                    checkDelete.click();
+    }
+*/
     @AfterMethod
     void AfterM(ITestResult testResult){
         System.out.println(testResult.getMethod().getDescription());
         System.out.println(testResult.isSuccess());
+
     }
 
     @AfterTest
     void AfterT()
     { driver.close();
     }
-    
+
 }
